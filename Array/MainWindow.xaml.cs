3f0e3cbf297242c93a48b4ea185cd4892c12497f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-08 12:50:48 +0100

    esercizio in classe 08/01/2020

--- Array/MainWindow.xaml.cs
+++ Array/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,42 @@ namespace Array
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] frasi = new string[5];int i;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnInserisci_Click(object sender, RoutedEventArgs e)
+        {
+            frasi[i] = TxtInserisci.Text;
+            i++;
+            if (i == 5)
+            {
+                BtnInserisci.IsEnabled = false;
+                BtnPubblica.IsEnabled = true;
+                BtnStampa.IsEnabled = true;
+            }
+            TxtInserisci.Clear();
+        }
+
+        private void BtnStampa_Click(object sender, RoutedEventArgs e)
+        {
+            for(int i = 0;i < frasi.Length; i++)
+            {
+                lblrisultato.Content += $"{frasi[i]}\n";
+            }
+        }
+
+        private void BtnPubblica_Click(object sender, RoutedEventArgs e)
+        {
+            StreamWriter w = new StreamWriter("pubblica.txt", false, Encoding.UTF8);
+            for (int i = 0; i < frasi.Length; i++) 
+            {
+                w.WriteLine( $"{frasi[i]}\n");
+            }
+            w.Flush();
+            w.Close();
         }
     }
 }
